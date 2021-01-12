eabab73fcb895fc63f76da61652332b8ea69b543
AguiarVicente <aguiar14564@gmail.com>
2021-01-12 11:22:13 -0300

    Introdução aos Módulos em Angular

---
+++ first-project/src/app/my-first/my-first.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-first-component',
+    template: `
+        <p>My first component in Angular</p>
+    `
+})
+
+export class MyFirstComponent{
+
+}
