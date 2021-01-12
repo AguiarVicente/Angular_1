20cff6fcd740a8e9488db968da07a8fb21e4ca77
AguiarVicente <aguiar14564@gmail.com>
2021-01-12 10:24:40 -0300

    Introdução ao TypeScript

---
+++ hello-typescript/main.js
@@ -0,0 +1,20 @@
+var minhaVar = 'minha variável';
+function minhFunc(x, y) {
+    return x + y;
+}
+// ES 6 ou ES 2015
+var num = 2;
+var PI = 3.14;
+var numero = [1, 2, 3];
+numero.map(function (valor) { return valor * 2; });
+numero.map(function (valor) { return valor * 2; });
+var Matematica = /** @class */ (function () {
+    function Matematica() {
+    }
+    Matematica.prototype.soma = function (x, y) {
+        return x + y;
+    };
+    return Matematica;
+}());
+var n1 = 'sdfasdf';
+n1 = 4;
