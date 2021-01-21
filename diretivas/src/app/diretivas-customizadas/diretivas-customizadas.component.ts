fb1aa682e6104cb798a626ad93fc7c0cc8c47c4a
AguiarVicente <aguiar14564@gmail.com>
2021-01-20 23:01:35 -0300

    Diretivas no Angular - Finalizado o módulo de diretivas com a ciração de uma estrutura customizada (ngElse)

--- diretivas/src/app/diretivas-customizadas/diretivas-customizadas.component.ts
+++ diretivas/src/app/diretivas-customizadas/diretivas-customizadas.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiretivasCustomizadasComponent implements OnInit {
 
+  mostrarCursos: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onMostrarCurso(){
+    this.mostrarCursos = !this.mostrarCursos;
   }
 
 }
