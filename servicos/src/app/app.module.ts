204891cb5ef82917bb3fc42c421aba5315ddf47c
AguiarVicente <aguiar14564@gmail.com>
2021-01-21 22:32:11 -0300

    Serviços no Angular

--- servicos/src/app/app.module.ts
+++ servicos/src/app/app.module.ts
@@ -3,17 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CursosComponent } from './cursos/cursos.component';
+import { CursosModule } from './cursos/cursos.module';
 import { CursosService } from './cursos/cursos.service';
+import { CriarCursoModule } from './criar-curso/criar-curso.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CursosComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CriarCursoModule,
+    CursosModule
   ],
   providers: [
     CursosService
--- servicos/src/app/cursos/cursos.service.ts
+++ servicos/src/app/cursos/cursos.service.ts
@@ -3,7 +3,17 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class CursosService {
 
+    private cursos: Array<string> = ['Angular', 'Java', 'Node'];
+
+    constructor(){
+        console.log('CursosService');
+    }
+
     getCursos(){
-        return ['Angular', 'Java', 'Node'];
+        return this.cursos;
+    }
+
+    addCurso( curso: string ){
+        this.cursos.push(curso);
     }
 }
