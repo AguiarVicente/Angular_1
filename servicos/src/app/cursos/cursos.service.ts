5e528e9b818711ff15bb105cd8ca8260b8c2c0b2
AguiarVicente <aguiar14564@gmail.com>
2021-01-21 23:06:30 -0300

    Serviços no Angular - Finzaliado

--- servicos/src/app/cursos/cursos.service.ts
+++ servicos/src/app/cursos/cursos.service.ts
@@ -1,19 +1,28 @@
-import { Injectable } from "@angular/core";
+import { Injectable, EventEmitter } from "@angular/core";
+
+import { LogService } from './../shared/log.service';
 
 @Injectable()
 export class CursosService {
 
+    emitirCursoCriado = new EventEmitter<string>();
+    static criouNovoCurso = new EventEmitter<string>();
+
     private cursos: Array<string> = ['Angular', 'Java', 'Node'];
 
-    constructor(){
+    constructor(private logService: LogService){
         console.log('CursosService');
     }
 
     getCursos(){
+        this.logService.consoleLog('Obtendo Lista de cursos');
         return this.cursos;
     }
 
     addCurso( curso: string ){
+        this.logService.consoleLog(`Criando um novo cursos ${curso}`);
         this.cursos.push(curso);
+        this.emitirCursoCriado.emit(curso);
+        CursosService.criouNovoCurso.emit(curso);
     }
 }
