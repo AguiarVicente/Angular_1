0ffcd57422849eb6b93ef5cd1117a4a251435aed
AguiarVicente <aguiar14564@gmail.com>
2021-01-24 21:38:50 -0300

    Rotas - Definindo e extraindo parâmetros de roteamento (queryParams)

--- rotas/src/app/cursos/cursos.component.ts
+++ rotas/src/app/cursos/cursos.component.ts
@@ -1,5 +1,8 @@
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+
 import { CursosService } from './cursos.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-cursos',
@@ -9,11 +12,30 @@ import { CursosService } from './cursos.service';
 export class CursosComponent implements OnInit {
 
   cursos: Array<any> = [];
+  pagina: number = 0;
+  inscricao: Subscription = new Subscription();
 
-  constructor(private cursosService: CursosService) { }
+  constructor(
+    private cursosService: CursosService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.cursos = this.cursosService.getCursos();
+
+    this.inscricao = this.route.queryParams.subscribe( (queryParams) => {
+      this.pagina = queryParams['pagina'];
+    })
+  }
+
+  ngOnDestroy(){
+    this.inscricao.unsubscribe();
+  }
+
+  proximaPagina(){
+    // this.pagina++;
+    this.router.navigate(['/cursos'], {queryParams: {'pagina': ++this.pagina}})
   }
 
 }
