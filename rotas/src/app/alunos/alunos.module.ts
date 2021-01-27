5242fd2881d9baa3a8f2c030c5fbe619f81012ee
AguiarVicente <aguiar14564@gmail.com>
2021-01-26 23:07:43 -0300

    Rotas - Desenvolvendo as telas filhas

--- rotas/src/app/alunos/alunos.module.ts
+++ rotas/src/app/alunos/alunos.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
@@ -5,15 +6,19 @@ import { AlunoFormComponent } from './aluno-form/aluno-form.component';
 import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
 import { AlunosRoutingModule } from './alunos-routing.module';
 import { AlunosComponent } from './alunos.component';
+import { AlunosService } from './alunos.service';
 
 @NgModule({
     imports: [
         CommonModule,
+        FormsModule,
         AlunosRoutingModule
     ],
     exports: [],
     declarations: [AlunosComponent, AlunoFormComponent, AlunoDetalheComponent],
-    providers: [],
+    providers: [
+        AlunosService
+    ],
 })
 
 export class AlunosModule {}
